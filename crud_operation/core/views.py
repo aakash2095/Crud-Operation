ece189fa27eb8c5f4aa04b063e210f542064ffa1
Aakash Soni <soniaakash5336@gmail.com>
2024-12-11 16:51:28 +0530

    update

--- crud_operation/core/views.py
+++ crud_operation/core/views.py
@@ -15,3 +15,19 @@ def base(request):
         cd=Crud.objects.all()
         return render (request,'core/base.html',{'crud':crud,'cd':cd})
 
+def delete(request,id):
+    if request.method == 'POST':
+        cd=Crud.objects.get(pk=id)
+        cd.delete()
+        return redirect('base')
+    
+def update(request,id):
+    if request.method == 'POST':
+        cd=Crud.objects.get(pk=id)
+        crud=Crudform(request.POST,instance=cd)
+        if crud.is_valid():
+            crud.save()
+    else:
+        cd=Crud.objects.get(pk=id)
+        crud=Crudform(instance=cd)
+    return render(request,'core/update.html',{'crud':crud})
